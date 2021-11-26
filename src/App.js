c7635bd881144bc285f8e51e270e18f77602c6c7
strawberryoolongtea <strawberrycream@kakao.com>
2021-11-27 02:45:44 +0900

    Start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  function getRandomIndex(len) {
+    return Math.floor(Math.random() * len);
+  }
+  const [menu, setMenu] = useState([
+    "곱창",
+    "족발",
+    "보쌈",
+    "치킨",
+    "피자",
+    "곱도리탕",
+    "떡볶이",
+    "닭강정",
+    "김치우동",
+    "라면",
+    "아이스크림",
+    "닭발",
+    "닭발",
+    "김치전",
+    "파전",
+    "골뱅이소면",
+    "닭꼬치",
+    "양꼬치",
+    "꿔바로우",
+    "오돌뼈",
+    "오뎅탕",
+    "낙곱새",
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">{menu[getRandomIndex(menu.length)]} 먹자!</div>
     </div>
   );
 }
